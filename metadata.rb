f04a2a6aa0da8bae279162101ab077ca818c040f
Mike McMahon <mmcm@comcast.net>
2014-01-21 15:06:05 -0500

    Update dependencies.

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,15 @@ maintainer_email 'distribution@foundationdb.com'
 license          'MIT'
 description      'Installs/Configures fdb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
+%w{ debian ubuntu centos redhat }.each do |os|
+  supports os
+end
+
+depends           "apt"
+depends           "java"
+
+attribute "fdb/cluster",
+  :display_name => "FDB Cluster definition",
+  :description => "fdb.cluster contents"
